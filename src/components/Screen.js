f49b25637a14c45fbe9a89350589de4cae652017
Vu Quach <zed.quach@gmail.com>
2022-09-30 22:09:08 +0700

    add scrollbar for screen

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -4,13 +4,31 @@ const Screen = ({ value, equation }) => {
       style={{
         border: "1px solid black",
         width: "100%",
-        height: "90px",
+        height: "110px",
         textAlign: "right",
         marginBottom: "10px",
       }}
     >
-      <div style={{height: "30px"}}>{equation}</div>
-      <div style={{height: "60px", fontSize:"50px"}}>{value}</div>
+      <div
+        style={{
+          height: "30px",
+        }}
+      >
+        {equation}
+      </div>
+      <div
+        style={{
+          direction: "rtl",
+          width: "100%",
+          height: "80px",
+          fontSize: "50px",
+          position: "relative",
+          overflowX: "scroll",
+          overflowY: "hidden",
+        }}
+      >
+        {value}
+      </div>
     </div>
   );
 };
